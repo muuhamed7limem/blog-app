70d184a3beb7c42d78e5cd675d0d94c1c40a6c0e
muuhamed7limem <muuhamedlimem@gmail.com>
2022-01-02 15:03:46 +0100

    8 commit

--- articles/static/script.js
+++ articles/static/script.js
@@ -1,6 +1,7 @@
 var title = document.querySelector('input[name=title]')
 var slug = document.querySelector('input[name=slug]')
 var pp = document.querySelector('.warn')
+var txtar = document.querySelector('textarea').maxLength
 
 
 function slugify(val){
@@ -17,6 +18,8 @@ title.addEventListener('keyup' , function(){
 
     slug.value = `${slugify(title.value)}${Math.floor(Math.random() * (9999999999 - 1000000000 + 1) + 1000000000)}`
 })
+
+txtar = 600;
 
 // ====================================================
 
